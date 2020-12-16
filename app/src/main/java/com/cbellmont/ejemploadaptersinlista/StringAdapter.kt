2771ada36f514c273b7ed45ecdd9b8f84ba1239b
Carlos <carlostena@centroafuera.es>
2020-12-16 13:35:28 +0100

    Solucion al EjercicioAdapterSinLista

--- app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
+++ app/src/main/java/com/cbellmont/ejemploadaptersinlista/StringAdapter.kt
@@ -1,5 +1,6 @@
 package com.cbellmont.ejemploadaptersinlista
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,8 @@ class StringAdapter() : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  
 
     class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
 
+    var numeroElementos = 100
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
         val twTextView = view.findViewById<TextView>(R.id.textView)
@@ -18,11 +21,27 @@ class StringAdapter() : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  
     }
 
     override fun getItemCount(): Int {
-        return 7
+        return numeroElementos
     }
 
     override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
-        holder.textView.text = "Position = $position"
+        if (position%2 == 0) {
+            holder.textView.text = "Position = $position"
+            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.colorPrimary))
+        } else {
+            holder.textView.text = "Soy Impar"
+            holder.textView.setBackgroundColor(holder.textView.context.getColor(R.color.colorAccent))
+        }
+        if (position == itemCount -1) {
+            holder.textView.text = "Añadir"
+            holder.root.setOnClickListener {
+                var toast = Toast.makeText(it.context, "Añadiendo...", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER,0,0)
+                toast.show()
+                numeroElementos++
+                notifyDataSetChanged()
+            }
+        }
     }
 
 }
